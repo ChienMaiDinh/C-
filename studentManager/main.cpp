6a11e036f492f6496b4f443ddbe0da5fe4c36995
ChienMaiDinh <vocabulary.723@gmail.com>
2019-06-11 11:12:33 +0700

    creat function menu

--- studentManager/main.cpp
+++ studentManager/main.cpp
@@ -16,6 +16,19 @@ void outPutInfor(Student *arrSd);			 //ham xuat thong tin
 void inPutInfor(int n,Student * arrSd);    	//ham nhap thong tin Student
 void menu();								//ham in thong tin lua chon
 
+void menu() {
+	
+	cout << "-------------------MENU-------------------\n";
+	cout << "		1.		input \n";
+	cout << "		2.		Display \n";
+	cout << "		3.		Save to file \n";
+	cout << "		4.		Load from file \n";
+	cout << "		0.		Exit \n";
+	cout << "-----------------------------------------\n";
+	cout << "Choose : \n";
+
+}
+
 int main(){
 	Student * arrSd = new Student[100]; 	//khoi tao mang student
 	
