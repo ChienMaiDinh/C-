86c50bd8b67263cd5548d44288189275c12a3095
ChienMaiDinh <vocabulary.723@gmail.com>
2019-06-11 11:05:37 +0700

    create main

---
+++ studentManager/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+
+
+using namespace std;
+
+
+struct Student {
+	int id;
+	string name;
+	float score;
+};
+
+void outPutInfor(Student *arrSd);			 //ham xuat thong tin 
+void inPutInfor(int n,Student * arrSd);    	//ham nhap thong tin Student
+void menu();								//ham in thong tin lua chon
+
+int main(){
+	
+	return 0;
+}
