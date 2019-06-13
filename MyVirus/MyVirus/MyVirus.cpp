36b2c368ffe2eda72b50d5ba4d513da50c64c070
ChienMaiDinh <vocabulary.723@gmail.com>
2019-06-13 11:19:18 +0700

    +add function get,set to MyVirus.cpp

--- MyVirus/MyVirus/MyVirus.cpp
+++ MyVirus/MyVirus/MyVirus.cpp
@@ -16,6 +16,20 @@ void MyVirus::LoadADNInformation(){
 void MyVirus::ReduceResistance(int medicine_resistance){
 }
 
+int MyVirus::Get_m_resistance() {
+	return this->m_resistance;
+}
 
+void MyVirus::Set_m_resistance(int resistance) {
+	this->m_resistance = resistance;
+}
+
+char * MyVirus::Get_m_dna() {
+	return this->m_dna;
+}
+
+void MyVirus::Set_m_dna(char * dna) {
+	this->m_dna = dna;
+}
 
 
