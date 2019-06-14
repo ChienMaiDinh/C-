b81383c0cb9eb1cf808c96a76b76226cbce976af
ChienMaiDinh <vocabulary.723@gmail.com>
2019-06-14 23:56:07 +0700

    +Add Source

--- MyVirus/MyVirus/Source.cpp
+++ MyVirus/MyVirus/Source.cpp
@@ -1,22 +1,25 @@
 #include "MyVirus.h"
 #include "Flu.h"
 #include <iostream>
+#include <stdio.h>
+#include <cstdlib>
+#include "Patient.h"
+#include <iostream>
 
-void main() {
-	MyVirus *a = new Flu();
-	a->Set_m_dna("asd");
-	std::cout << a->Get_m_dna();
-	a->Set_m_resistance(10);
-	a->LoadADNInformation();
-	std::cout << "uyuyu";
-	std::cout << a->Get_m_dna();
-	Flu * b=new Flu();
-	a = b;
-
-	a->DoBorn();
-
-
-	
-
-	system("pause");
+int main()
+{
+	Patient p;
+	char t = 0;
+	while (p.Get_m_state() == 1)
+	{
+		printf("Take Medicine (0 = NO, 1 = YES");
+		std::cin>>&t;
+		if (t == 1)
+		{
+			int min = 1;
+			int max = 60;
+			int medicine_resistance = min + (rand() % (int)(max - min + 1));
+			p.TakeMedicine(medicine_resistance);
+		}
+	}
 }
