8198ac00022f1aa64e611e6f666a6365b651499f
ChienMaiDinh <vocabulary.723@gmail.com>
2019-06-14 19:57:48 +0700

    +Add function DoBorn in Dengue

--- MyVirus/MyVirus/Dengue.cpp
+++ MyVirus/MyVirus/Dengue.cpp
@@ -1,4 +1,7 @@
 #include "Dengue.h"
+#include <string>
+#include "Patient.h"
+#include "MyVirus.h"
 
 
 
@@ -23,8 +26,13 @@ void Dengue::Set_m_protein(std::string protein){
 	this->m_protein = protein;
 }
 
-void Dengue::DoBorn()
-{
-	
+void Dengue::DoBorn(){
+	LoadADNInformation();
+	std::string temp[3] = { "NS3","NS5","E" };
+	this->m_protein = temp[rand() % 3];
+}
+
+void Dengue::Doclone(){
+
 }
 
