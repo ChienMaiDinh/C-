43a990335bdd9b715256f6218139f59abb3dc81d
ChienMaiDinh <vocabulary.723@gmail.com>
2019-06-14 14:53:39 +0700

    -Update remove Myvirus * next

--- MyVirus/MyVirus/MyVirus.h
+++ MyVirus/MyVirus/MyVirus.h
@@ -6,17 +6,15 @@ class MyVirus
 protected :								
 	char* m_dna;						 //to store the order of acid nucleic sequence (A, T, G,X)
 	int m_resistance;					//the virus resistance ability (10 – 30)
-	MyVirus * next;
 public:
 	MyVirus();
 	~MyVirus();
 	MyVirus(const MyVirus & myVirus);
-	MyVirus(char * const dna, int resistance, MyVirus * const next = 0);
 	void LoadADNInformation();				//Load DNA information from a ATGX.bin and store it in m_dna.
 	int ReduceResistance(int medicine_resistance);
 
 	int Get_m_resistance();
-	void Set_m_resistance(int resistanc	e);
+	void Set_m_resistance(int resistance);
 	char* Get_m_dna();
 	void Set_m_dna(char * dna);
 
