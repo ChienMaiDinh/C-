2eb2cc014a933f7b24808917e0b228261e80bdee
ChienMaiDinh <vocabulary.723@gmail.com>
2019-06-14 18:09:06 +0700

    -Update function DoBorn in Flu.cpp

--- MyVirus/MyVirus/Flu.cpp
+++ MyVirus/MyVirus/Flu.cpp
@@ -12,6 +12,11 @@ Flu::Flu(char * dna, int resistance, int color) : MyVirus(m_dna, m_resistance){
 
 void Flu::DoBorn(){
 	LoadADNInformation();
+	if(rand() % 2 == 1) {
+		this->m_color = 0x0000ff;
+	}else {
+		this->m_color = 0xff0000;
+	}
 }
 
 void Flu::Set_m_color(int color){
@@ -23,11 +28,10 @@ int Flu::Get_m_color()
 	return this->m_color;
 }
 
-Flu::Flu()
-{
+Flu::Flu(){
+	DoBorn();
 }
 
 
-Flu::~Flu()
-{
+Flu::~Flu(){
 }
